0cce8f57354ab5ea517043f0dfaa2698549fdd94
jetpackgone <jetpackgone.dev@gmail.com>
2020-11-30 19:27:54 -0500

    Adding campfire files

---
+++ Fire.gd
@@ -0,0 +1,11 @@
+extends Sprite
+
+onready var player: AnimationPlayer = $AnimationPlayer
+onready var sparkParticles: Particles2D = $Sparks
+onready var smokeParticles: Particles2D = $Smoke
+
+func _ready():
+	player.play("burn")
+	sparkParticles.emitting = true
+	smokeParticles.emitting = true
+	sparkParticles.lifetime = 0.5
